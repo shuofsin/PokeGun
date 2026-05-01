d1f1547e0676b9d1835ff0a217a393e2a083ad00
shuofsin <sinhah239@gmail.com>
2026-04-30 19:46:25 -0600

    Hitscan works. Needs polish and bug fix

--- scenes/basic_shotgun.gd
+++ scenes/basic_shotgun.gd
@@ -3,14 +3,14 @@ extends Weapon
 func _shoot() -> void: 
 	var offset: float = 15
 	if weapon_sprite:
-		offset = weapon_sprite.texture.get_width() * weapon_sprite.scale.x # + weapon_sprite.position.x
+		offset = weapon_sprite.texture.get_width() * weapon_sprite.scale.x
 	
 	for i in range(10):
 		var new_bullet = bullet_scene.instantiate()
 		new_bullet.global_rotation = global_rotation + randf_range(-0.3, 0.3)
 		new_bullet.global_position = Vector2(
-			global_position.x + offset * cos(rotation),
-			global_position.y + offset * sin(rotation)
+			global_position.x + offset * cos(rotation) + randf_range(-5, 5),
+			global_position.y + offset * sin(rotation) + randf_range(-5, 5)
 		)
 		get_tree().current_scene.add_child(new_bullet)
 	
--- scripts/hitscan_projectile.gd
+++ scripts/hitscan_projectile.gd
@@ -3,29 +3,26 @@ class_name HitscanProjectile
 
 @export var attack: Attack
 @onready var ray_cast_2d: RayCast2D = $RayCast2D
+@onready var line_2d: Line2D = %Line2D
 
 var collision_point: Vector2 
 var is_bullet_drawn: bool = false
 
 func _ready() -> void: 
-	collision_point = to_global(ray_cast_2d.target_position)
+	collision_point = ray_cast_2d.target_position
 
 func _physics_process(_delta: float) -> void:
+	# check for collision
 	if ray_cast_2d.is_colliding():
-		collision_point = ray_cast_2d.get_collision_point()
+		collision_point = to_local(ray_cast_2d.get_collision_point())
 		if ray_cast_2d.get_collider() is HealthBoxComponent: 
 			var health_box_component: HealthBoxComponent = ray_cast_2d.get_collider()
 			health_box_component.damage(attack)
 			ray_cast_2d.enabled = false
 	
-	if !is_bullet_drawn: 
-		var bullet_line: Line2D = Line2D.new()
-		bullet_line.global_position = global_position
-		bullet_line.add_point(global_position)
-		bullet_line.add_point(collision_point)
-		bullet_line.default_color = Color(0.968, 0.806, 0.396, 1.0)
-		add_child(bullet_line)
-		# var tween_fade: Tween = create_tween()
-		# tween_fade.tween_property(self, "modulate:a", 0.0, 0.2)
-		# tween_fade.finished.connect(queue_free)
-		is_bullet_drawn = true
+	# draw and fade bullet trail
+	line_2d.set_point_position(1, collision_point)
+	line_2d.visible = true
+	var fade_tween = create_tween()
+	fade_tween.tween_property(self, "modulate:a", 0, 0.1)
+	fade_tween.finished.connect(queue_free)
